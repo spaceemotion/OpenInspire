9544c5510c6666e4c81c5ab69bc81135c12a0df2
SpaceEmotion <spaceemotion@me.com>
2012-05-07 20:47:00 +0200

    Added some en_US language strings
    Added some default en_US language strings

--- system/languages/en_US.php
+++ system/languages/en_US.php
@@ -4,5 +4,17 @@
   *  Copyright (c) 2012 - Verexa
   */
 
-
+$lang["en_US"]["yes"]		= "Yes";
+$lang["en_US"]["no"]		= "No";
+$lang["en_US"]["save"]		= "Save";
+$lang["en_US"]["edit"]		= "Edit";
+$lang["en_US"]["cancel"]	= "Cancel";
+$lang["en_US"]["language"]	= "Language";
+$lang["en_US"]["change"]	= "Change";
+$lang["en_US"]["reset"]		= "Reset";
+$lang["en_US"]["default"]	= "Default";
+$lang["en_US"]["enabled"]	= "Enabled";
+$lang["en_US"]["disabled"]	= "Disabled";
+$lang["en_US"]["abort"]		= "Abort";
+$lang["en_US"]["confirm"]	= "Confirm";
 ?>
